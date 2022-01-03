7932d72034b49ec195c5e681eee181d974c14765
neel <neelpatelshah@gmail.com>
2022-01-03 01:42:29 -0500

    afbafbad

--- src/App.js
+++ src/App.js
@@ -8,13 +8,13 @@ function App() {
       <header className="App-header">
         <div style={{ display: "flex", flexDirection: "row", alignItems: "center"}}>
           <h4>fANTasy </h4>
-          <img src={logo} style={{width: 50, height: 50, margin: 5 }}/>
+          <img src={logo} style={{width: 50, height: 50, margin: 5 }} alt="logo"/>
           <h4> football</h4>
         </div>
         <h2>
           2021-2022 champ: Pee Piggins
         </h2>
-        <img src={neel} className="App-logo" alt="logo" />
+        <img src={neel} className="App-logo" alt="neel" />
         <h5>
           Runner Up: Tyler Stock
         </h5>
